398f16ed2614398234932185bf8572c2a0948a6a
emiryalcinkaya <emirylk06@gmail.com>
2026-05-15 17:30:14 +0200

    Exercise

---
+++ pizza_loop.py
@@ -0,0 +1,9 @@
+pizzas = ['Margherita', 'Quattro Formaggi', 'Parmigiana']
+
+message = "My favorite pizzas: "
+print(message)
+
+for pizza in pizzas: 
+	print(pizza.title() + " is one of my favorite pizzas!")
+
+print("Those are my favorite pizzas.")
