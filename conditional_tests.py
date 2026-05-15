42b83bf519807f657288659376acbd7fd91c0efd
emiryalcinkaya <emirylk06@gmail.com>
2026-05-15 22:02:29 +0200

    Exercise

---
+++ conditional_tests.py
@@ -0,0 +1,35 @@
+# Exercise 5-1: Conditional Tests
+
+car = 'subaru'
+number = 10
+city = 'Berlin'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs number > 5? I predict True.")
+print(number > 5)
+
+print("\nIs number < 5? I predict False.")
+print(number < 5)
+
+print("\nIs city == 'Berlin'? I predict True.")
+print(city == 'Berlin')
+
+print("\nIs city == 'Ankara'? I predict False.")
+print(city == 'Ankara')
+
+print("\nIs number >= 10? I predict True.")
+print(number >= 10)
+
+print("\nIs number != 10? I predict False.")
+print(number != 10)
+
+print("\nIs car != 'bmw'? I predict True.")
+print(car != 'bmw')
+
+print("\nIs city != 'Berlin'? I predict False.")
+print(city != 'Berlin')
