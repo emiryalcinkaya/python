950ed5d9460fd710f9559c8e0337f6d33240b08c
emiryalcinkaya <emirylk06@gmail.com>
2026-05-15 17:48:38 +0200

    Exercise

---
+++ animals_loop.py
@@ -0,0 +1,9 @@
+animals = ['elephant', 'rhino', 'lion', 'crocodile', 'tiger'] # List of animals
+
+message = "The wild animals in our zoo:"
+print(message) # Printing wild animals in zoo
+
+for animal in animals: # Using for loop
+	print(animal.title() + " is a wild animal and cannot be kept as a pet.") # Printing message for loop
+
+print("All of these animals are wild animals.") 
